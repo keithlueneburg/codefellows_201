5b9e27137f7e06d2f86a3cea4d1a995fedb7ea1f
Keith Lueneburg <keith.lueneburg@gmail.com>
2017-09-05 22:04:51 -0700

    Testing JS script ideas

--- js/breed_find.js
+++ js/breed_find.js
@@ -1,1 +1,8 @@
-//todo
+// Get list of all available breeds
+var breeds = document.getElementsByClassName('breed');
+for (b of breeds) {
+  if(true || b.hasAttribute('traits')
+  && b.getAttribute('traits').indexOf('large') != -1){
+    b.hidden = false;
+  }
+}
